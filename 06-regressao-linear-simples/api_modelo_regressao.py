a5e368cfbff743919bf071c9cee9e5d98fa1429e
Renan Oliveira <rotrenanoliveira@gmail.com>
2025-11-15 18:45:14 -0300

    módulo 06 - Regressão Linear Simples

---
+++ 06-regressao-linear-simples/api_modelo_regressao.py
@@ -0,0 +1,27 @@
+import joblib
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Criar uma instância do FastAPI
+app = FastAPI()
+
+
+# Criar uma classe que terá os dados do request body para a API
+class request_body(BaseModel):
+    horas_estudo: float
+
+
+# Carregar modelo para realizar a predição
+modelo_pontuacao = joblib.load("./modelo_regressao.pkl")
+
+
+@app.post("/predict")
+def predict(data: request_body):
+    # Preparar os dados para a predição
+    input_feature = [[data.horas_estudo]]
+
+    # Realizar a predição
+    y_pred = modelo_pontuacao.predict(input_feature)[0].astype(int)
+
+    return {"pontuacao_teste": y_pred.tolist()}
